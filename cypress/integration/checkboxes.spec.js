fb1d5152f95fbfb566b02393fc8f8b752f14a123
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-20 17:03:16 -0300

    adjust_test: checkboxes validations

--- cypress/integration/checkboxes.spec.js
+++ cypress/integration/checkboxes.spec.js
@@ -1,13 +1,25 @@
-it('Deve marcar 5 checkboxes', () => {
+describe('Quando selecionar 5 filmes, deve:', () => {
   const movies = ['cap', 'capmarvel', 'ironman', 'hulk', 'ironman2', 'thor', 'avengers', 'ironman3', 'tho2', 'cap2', 'guardians',
     'guardians2', 'avengers2', 'antman', 'cap3', 'blackwidow', 'spider', 'strange', 'blackpanther', 'thor3', 'antman2', 'avengers3',
     'avengers4', 'spider2', 'spider3', 'shangchi', 'eternals'];
 
   const moviesToSelect = 5;
   const chosenMovies = movies.sort(() => 0.5 - Math.random()).slice(0, moviesToSelect);
-  cy.visit('https://training-wheels-qaninja.herokuapp.com/checkboxes');
+  const unselectMovies = movies.filter((movie) => !chosenMovies.includes(movie));
 
-  chosenMovies.forEach((movie) => {
-    cy.get(`input[name=${movie}]`).click().should('be.checked');
+  before(() => {
+    cy.visit('https://training-wheels-qaninja.herokuapp.com/checkboxes');
+  });
+
+  it('Marcar os 5 filmes escolhidos', () => {
+    chosenMovies.forEach((movie) => {
+      cy.get(`input[name=${movie}]`).click().should('be.checked');
+    });
+  });
+
+  it('Manter desmarcados os demais filmes', () => {
+    unselectMovies.forEach((movie) => {
+      cy.get(`input[name=${movie}]`).should('not.be.checked');
+    });
   });
 });
