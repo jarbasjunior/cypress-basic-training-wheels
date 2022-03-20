4e72c8edbdfe2830051489a14072b75d8cc96621
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-20 14:18:04 -0300

    test: home page validations

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,19 @@
+describe('Ao visitar home, página deve:', () => {
+  before(() => {
+    cy.visit('https://training-wheels-qaninja.herokuapp.com');
+  });
+
+  it('Exibir o título Training Wheels | QAninja', () => {
+    cy.title().should('eq', 'Training Wheels | QAninja');
+  });
+
+  it('Exibir logo Training Wheel da QA Ninja', () => {
+    cy.get('a > img').should('have.attr', 'src', '/img/bg-hero-v2.png');
+  });
+
+  it('Exibir menu de ações em páginas Web com 17 items', () => {
+    cy.get('ul[class=menu-list] li')
+      .should('be.visible')
+      .and('have.length', 17);
+  });
+});
