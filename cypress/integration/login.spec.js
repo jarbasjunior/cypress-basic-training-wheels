4108f1367b6d239e070dc55d2efb37acf02b0095
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-20 14:56:27 -0300

    test: login successfully

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,10 @@
+it('Deve realizar login com sucesso', () => {
+  const msgUserExpected = 'Olá Papito, bem-vindo ao Orkut';
+  const msgWelcomeExpected = 'Em breve você poderá participar de comunidades, adicionar amigos e deixar um Scraps. hahahahah';
+  cy.visit('https://training-wheels-qaninja.herokuapp.com/login');
+  cy.get('#nickId').type('papitorocks');
+  cy.get('#passId').type('pwd123');
+  cy.get('button[type="submit"]').click();
+  cy.get('h3[class="title is-4"]').should('have.text', msgUserExpected);
+  cy.get('p[class="subheader"]').should('have.text', msgWelcomeExpected);
+});
