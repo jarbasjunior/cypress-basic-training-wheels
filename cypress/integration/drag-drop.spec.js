196957a6741bbdd647566ce04bcd132eefc28e49
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-20 17:54:40 -0300

    test: drag and drop validations

---
+++ cypress/integration/drag-drop.spec.js
@@ -0,0 +1,17 @@
+it('Deve arrastar a imagem do Cypress da coluna Ruby para a do Node', () => {
+  cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop');
+  const dataTransfer = new DataTransfer();
+
+  cy.get('img[alt=Cypress]').trigger('dragstart', { dataTransfer });
+  cy.get('.nodejs figure[draggable=true]').trigger('drop', { dataTransfer });
+  cy.get('img[alt=Cypress]').should('have.attr', 'src', '/img/cypress.png');
+});
+
+it('Deve arrastar a imagem do Robot da coluna do PHP para a do Python', () => {
+  cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop');
+  const dataTransfer = new DataTransfer();
+
+  cy.get('img[alt="Robot Framework"]').trigger('dragstart', { dataTransfer });
+  cy.get('.python figure[draggable=true]').trigger('drop', { dataTransfer });
+  cy.get('img[alt="Robot Framework"]').should('have.attr', 'src', '/img/robot.png');
+});
