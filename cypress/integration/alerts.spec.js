3ee120f5e134607cb2054daf20a76bed3bb0b7e3
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-23 17:27:18 -0300

    test: prompt validation

--- cypress/integration/alerts.spec.js
+++ cypress/integration/alerts.spec.js
@@ -1,22 +1,31 @@
-it('Deve validar mensagem do alert', () => {
-  cy.visit('/javascript_alerts');
-  cy.contains('button', 'Alerta').click();
-  cy.on('window:alert', (result) => (
-    expect(result).to.eq('Isto é uma mensagem de alerta')
-  ));
-  cy.get('#result').should('have.text', 'Você ativou um alerta JS');
-});
+describe('Testes de alerts, confirms e prompts', () => {
+  before(() => cy.visit('/javascript_alerts'));
 
-it('Deve confirmar a solicitação', () => {
-  cy.visit('/javascript_alerts');
-  cy.contains('button', 'Confirma').click();
-  cy.on('window:confirm', () => true);
-  cy.get('#result').should('have.text', 'Mensagem confirmada');
-});
+  it('Deve validar mensagem do alert', () => {
+    cy.contains('button', 'Alerta').click();
+    cy.on('window:alert', (result) => (
+      expect(result).to.eq('Isto é uma mensagem de alerta')
+    ));
+    cy.get('#result').should('have.text', 'Você ativou um alerta JS');
+  });
 
-it('Deve cancelar a solicitação', () => {
-  cy.visit('/javascript_alerts');
-  cy.contains('button', 'Confirma').click();
-  cy.on('window:confirm', () => false);
-  cy.get('#result').should('have.text', 'Mensagem não confirmada');
+  it('Deve confirmar a solicitação', () => {
+    cy.contains('button', 'Confirma').click();
+    cy.on('window:confirm', () => true);
+    cy.get('#result').should('have.text', 'Mensagem confirmada');
+  });
+
+  it('Deve cancelar a solicitação', () => {
+    cy.contains('button', 'Confirma').click();
+    cy.on('window:confirm', () => false);
+    cy.get('#result').should('have.text', 'Mensagem não confirmada');
+  });
+
+  it('Deve confirmar prompt', () => {
+    cy.window().then(($win) => {
+      cy.stub($win, 'prompt').returns('Robô');
+      cy.contains('button', 'Prompt').click();
+    });
+    cy.get('#result').should('have.text', 'Olá, Robô');
+  });
 });
